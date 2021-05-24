d4ba2ff0d9cf1449892330969e33cf06926d1c9e
Willy-Chuang <ws18950914@gmail.com>
2021-05-24 22:25:39 +0800

    Create User object regarding the UI requirements

---
+++ app/src/main/java/com/willychuang/access/data/User.kt
@@ -0,0 +1,16 @@
+package com.willychuang.access.data
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class User(
+    @Json(name = "login") val login: String,
+    @Json(name = "avatar_url") val avatarUrl: String? = "",
+    @Json(name = "site_admin") val siteAdmin: Boolean,
+    @Json(name = "name")val name: String? = "",
+    @Json(name = "bio")val bio: String? = "",
+    @Json(name = "location")val location: String? = "",
+    @Json(name = "blog")val blog: String? = ""
+) : Parcelable
